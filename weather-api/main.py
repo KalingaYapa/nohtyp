d12855ad2797da10c959963c82deda7c6be92d88
KalingaY <y.kalinga@directfn.com>
2022-08-11 02:21:13 +0530

    image api added

--- weather-api/main.py
+++ weather-api/main.py
@@ -3,11 +3,16 @@ import requests
 API_KEY = '389c82bf0ad6f18c71141214f1154e77'
 CITY = 'London'
 API_URL = f'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}'
+IMAGE_API_KEY = '29188269-b83105c6bfe5ac1814d3944bb'
 
 response = requests.get(API_URL)
 response_json = response.json()
 sky_mode = response_json["weather"][0]["main"]
 
-print(sky_mode)
+IMAGE_API_URL = f'https://pixabay.com/api/?key={IMAGE_API_KEY}&q={sky_mode}&image_type=photo'
+
+image_response = requests.get(IMAGE_API_URL)
+
+#print(image_response.json())
 
 
